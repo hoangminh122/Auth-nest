e99b44a840f83d5b0a61827b0090c5b574a727ef
hoangminh122 <hoangminh12297@gmail.com>
2022-06-30 13:03:03 +0700

    fb(2): api logout

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,27 +5,37 @@ import { AuthModule } from './modules/auth/auth.module';
 import { AuthService } from './modules/auth/auth.service';
 import { DatabaseModule } from './modules/database/database.module';
 import { UserModule } from './modules/users/users.module';
-import { BlackListMiddleware } from './shared/middleware/black-list.middleware';
+import { BlackListInterceptor } from './shared/interceptor/black-list.interceptor';
+import { ConfigService } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     AuthModule,
     UserModule,
     DatabaseModule,
-    CacheModule.register()
+     CacheModule.register({
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        ttl: process.env.CACHE_TTL,
+      }),
+    }),
   ],
   controllers: [AuthController],
   providers: [
     {
-    provide: APP_INTERCEPTOR,
-    useClass: CacheInterceptor,
+      provide: APP_INTERCEPTOR,
+      useClass: BlackListInterceptor,
     }
   ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(BlackListMiddleware)
-      .forRoutes('*');
+    console.log("MiddlewareConsumer")
+    // consumer
+    //   .apply(BlackListInterceptor)
+    //   .forRoutes('*');
   }
 }
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule, forwardRef, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { JwtStrategy } from '../auth/jwt/jwt-strategy';
 import { JwtModule } from '@nestjs/jwt';
@@ -6,10 +6,30 @@ import { jwtConstants } from '../auth/jwt/jwt-secret.constant';
 import { UserService } from './users.service';
 import { userRepository } from '../database/repository.database.provider';
 import { DatabaseModule } from '../database/database.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { AuthModule } from '../auth/auth.module';
+import { AuthService } from '../auth/auth.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
+import { BlackListInterceptor } from 'src/shared/interceptor/black-list.interceptor';
 
 @Module({
-  imports: [DatabaseModule],
-  providers: [UserService, userRepository],
+  imports: [
+    DatabaseModule,
+    CacheModule.register({
+      imports: [AuthModule],
+      inject: [ConfigService],
+      store: redisStore
+    }),
+  ],
+  providers: [
+    UserService,
+    userRepository,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: BlackListInterceptor,
+    }  
+  ],
   controllers: [UsersController],
   exports: [UserService],
 })
